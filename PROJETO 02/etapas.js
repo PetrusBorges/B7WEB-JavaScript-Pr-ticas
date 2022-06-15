64dc96b2395c7f7aece516f2c385fe1f1a9d7d01
PetrusBorges <contatopetrusborges@gmail.com>
2022-06-15 00:02:32 -0300

    Inclemento de arquivos js

---
+++ PROJETO 02/etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    título: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        nome: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos: [{ url: '04.jpg', legenda: 'Vereador' }]
+      },
+      {
+        numero: '77222',
+        nome: 'Beltrano da Silva',
+        partido: 'DEFG',
+        fotos: [{ url: '05.jpg', legenda: 'Vereador' }]
+      }
+    ]
+  },
+  {
+    título: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        nome: 'Ciclano',
+        partido: 'ABC',
+        vice: 'Cic',
+        fotos: [
+          { url: '03.jpg', legenda: 'Prefeito' },
+          { url: '04.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+      {
+        umero: '84',
+        nome: 'Zulano',
+        partido: 'QWERTY',
+        vice: 'Zul',
+        fotos: [
+          { url: '05.jpg', legenda: 'Prefeito' },
+          { url: '06.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      }
+    ]
+  }
+]
